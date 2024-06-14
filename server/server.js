f252a54e872f963195eeb28a7832b323b31f318d
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-06-14 21:50:29 +0700

    done connect db

--- server/server.js
+++ server/server.js
@@ -1,11 +1,42 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+const bodyParser=require('body-parser');
+const session = require("express-session");
+// const passport=require('./src/helper/passport');
+const { connect } = require("./src/config/db");
 
-app.get('/', (req, res) => {
-    res.send('Hello from the server!');
+var app = express();
+
+connect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: "cnttvietnhatk17",
+  resave: false,
+  saveUninitialized: true,
+}));
+
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+
+// app.use("/", require("./src/routes"));
+
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
 });
+
+module.exports = app;
