09119f317b5450cba8915db59732b599f9b30df1
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-06-16 08:58:45 +0700

    done test set up api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var createError = require("http-errors");
 var express = require("express");
 var cookieParser = require("cookie-parser");
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 const session = require("express-session");
-// const passport=require('./src/helper/passport');
-const cors=require('cors');
+const cors = require('cors');
 const { connect } = require("./src/config/db");
+const path = require('path');
 
 var app = express();
 
@@ -29,13 +29,13 @@ app.use(cors({
   optionsSuccessStatus: 204
 }));
 
-app.use(cors());
+app.use("/api", require("./src/routes"));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-
-// app.use("/", require("./src/routes"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -45,7 +45,59 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render("error");
+  res.json({ error: err.message }); 
 });
 
-module.exports = app;
+// Server setup
+var http = require('http');
+var debug = require('debug')('myapp:server');
+
+var port = normalizePort(process.env.PORT || '8000');
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+  console.log('Server is running on port ' + bind);
+}
---
+++ server/src/routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// Setting layout and handling routes
+const routes = [
+    // { path: '/home', handler: require('./home') },
+    // { path: '/blog',  handler: require('./blog') },
+    { path: '/contact',  handler: require('./contact') },
+];
+
+routes.forEach(route => {
+    router.use(route.path,route.handler);
+});
+
+
+// Error handling middleware
+router.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('server error!');
+});
+
+module.exports = router;
